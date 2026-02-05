95347ac859ce0f4cc4647478e03dbb3181307f3a
Justin Swenson <justinswenson111@gmail.com>
2026-02-05 11:35:43 -0700

    Better pathfinding and world collision

--- new-game-project/enemy.gd
+++ new-game-project/enemy.gd
@@ -3,21 +3,25 @@ extends CharacterBody2D
 @onready var player = get_tree().get_first_node_in_group("player")
 
 var chasing := false
-var speed := 120.0
+var speed := 60.0
 
 func _ready():
 	agent.target_position = global_position
+	
 
 func _physics_process(delta):
+	
+	agent.target_position = player.global_position
 	if not chasing:
 		return
 
-	if agent.is_navigation_finished():
-		return
+	# Update target every tick
+	agent.target_position = player.global_position
 
+	# Get next point on the path
 	var next_pos = agent.get_next_path_position()
-	var direction = (next_pos - global_position).normalized()
 
+	var direction = (next_pos - global_position).normalized()
 	velocity = direction * speed
 	move_and_slide()
 
